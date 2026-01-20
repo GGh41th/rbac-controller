9b6b5cd99bdd940416ccf2de7f7f25d889972085
Ghaith <gtari20ghaith33@gmail.com>
2026-01-20 21:21:03 +0100

    stop defaulting namespaces in the subject field
    We used to default the namespaces field to the default namespace, this
    would cause unwanted results when specifying a ns Selector or a ns
    MatchExpression (a service account in that ns will be present in the
    binding). To avoid such issues, users will have to specify the default
    ns explicitely if they want to.

--- api/v1alpha1/rbacrule_types.go
+++ api/v1alpha1/rbacrule_types.go
@@ -29,29 +29,29 @@ var (
 	ServiceAccount SubjectType = "ServiceAccount"
 )
 
-// we did not embedd the rbac.Subject type because we don't need the namespace
-// field.
+// +kubebuilder:validation:XValidation:rule="(has(self.namespaces) || has(self.nameSpaceSelector) || has(self.namespaceMatchExpression))",message="at least one namespace must be specified"
 type Subject struct {
 	// +required
 	Kind SubjectType `json:"kind"`
 	// +required
 	Name string `json:"name"`
-	// +required
-	// +kubebuilder:default={"default"}
-	Namespaces []string `json:"namespaces"`
+	// +optional
+	Namespaces []string `json:"namespaces,omitempty"`
 	// +optional
 	NameSpaceSelector metav1.LabelSelector `json:"nameSpaceSelector,omitempty"`
+	// +optional
+	NamespaceMatchExpression string `json:"namespaceMatchExpression,omitempty"`
 	// +optional
 	CreateSA bool `json:"createSA,omitempty"`
 }
 
 // +kubebuilder:validation:XValidation:rule="(has(self.namespaces) || has(self.nameSpaceSelector) || has(self.namespaceMatchExpression))",message="at least one namespace must be specified"
-// +kubebuilder:validation:XValidation:rule="(has(self.role) || has(self.clusterRole) || has(self.namespaceMatchExpression))",message="at least one role must be specified"
+// +kubebuilder:validation:XValidation:rule="(has(self.role) || has(self.clusterRole))",message="at least one role must be specified"
 type RoleBinding struct {
 	// +optional
-	Role string `json:"role"`
+	Role string `json:"role,omitempty"`
 	// +optional
-	ClusterRole string `json:"clusterRole"`
+	ClusterRole string `json:"clusterRole,omitempty"`
 	// +optional
 	Namespaces []string `json:"namespaces,omitempty"`
 	// +optional
@@ -65,25 +65,16 @@ type ClusterRoleBinding struct {
 	ClusterRole string `json:"clusterRole"`
 }
 
+// +kubebuilder:validation:XValidation:rule="(has(self.roleBindings) || has(self.clusterRoleBindings))",message="RoleBindings or ClusterRoleBindings should be specified"
 type Binding struct {
 	// +required
 	Name string `json:"name"`
 	// +required
 	Subjects []Subject `json:"subjects"`
 	// +optional
-	RoleBindings []RoleBinding `json:"roleBindings"`
+	RoleBindings []RoleBinding `json:"roleBindings,omitempty"`
 	// +optional
-	ClusterRoleBindings []ClusterRoleBinding `json:"clusterRoleBindings"`
-	// start Btime specify the binding creation time , refer to the docs for
-	// possible use cases.
-	// +optional
-	// +kubebuilder:validation:Format="date-time"
-	StartTime metav1.Time `json:"startTime,omitempty"`
-	// start time specify the binding creation time , refer to the docs for
-	// possible use cases.
-	// +optional
-	// +kubebuilder:validation:Format="date-time"
-	EndTime metav1.Time `json:"endTime,omitempty"`
+	ClusterRoleBindings []ClusterRoleBinding `json:"clusterRoleBindings,omitempty"`
 }
 
 // RBACRuleSpec defines the desired state of RBACRule
@@ -94,12 +85,12 @@ type RBACRuleSpec struct {
 	// binding will override it.
 	// +optional
 	// +kubebuilder:validation:Format="date-time"
-	StartTime metav1.Time `json:"startTime,omitempty"`
+	StartTime metav1.Time `json:"startTime,omitempty,omitzero"`
 	// If defined it will apply to all bindings. Specifying it at individual
 	// binding will override it.
 	// +optional
 	// +kubebuilder:validation:Format="date-time"
-	EndTime metav1.Time `json:"endTime,omitempty"`
+	EndTime metav1.Time `json:"endTime,omitempty,omitzero"`
 }
 
 // RBACRuleStatus defines the observed state of RBACRule.
--- cmd/controller-manager/app/options/options.go
+++ cmd/controller-manager/app/options/options.go
@@ -13,15 +13,21 @@ type ControllerManagerOptions struct {
 	SecureMetrics        bool
 	EnableHTTP2          bool
 	ProbeBindAddress     string
+	WebhookCertPath      string
+	WebhookCertName      string
+	WebhookCertKey       string
 }
 
 func (c *ControllerManagerOptions) Addflags(fs *pflag.FlagSet) {
-	fs.StringVar(&c.MetricsAddr, "metricsAddr", ":8080", "the address that the metrics server should bind to")
-	fs.StringVar(&c.MetricsCertPath, "metricsCertPath", "/tmp/k8s-metrics-server/serving-certs", "the directory that contains the metrics server key and certificate")
-	fs.StringVar(&c.MetricsCertName, "metricsCertName", "tls.crt", "the metrics server certificate name")
-	fs.StringVar(&c.MetricsCertKey, "metricsCertKey", "tls.key", "the metrics server key name")
-	fs.StringVar(&c.ProbeBindAddress, "ProbeBindAddress", "", "the TCP address that the manager should bind to")
-	fs.BoolVar(&c.EnableLeaderElection, "enableLeaderElection", false, "enable leader election for the controller manager")
+	fs.StringVar(&c.MetricsAddr, "metrics-bind-address", ":8080", "the address that the metrics server should bind to")
+	fs.StringVar(&c.MetricsCertPath, "metrics-cert-path", "/tmp/k8s-metrics-server/serving-certs", "the directory that contains the metrics server key and certificate")
+	fs.StringVar(&c.MetricsCertName, "metrics-cert-name", "tls.crt", "the metrics server certificate name")
+	fs.StringVar(&c.MetricsCertKey, "metrics-cert-key", "tls.key", "the metrics server key name")
+	fs.StringVar(&c.ProbeBindAddress, "pprof-bind-address", "", "the TCP address that the manager should bind to for serving pprof")
+	fs.StringVar(&c.WebhookCertPath, "webhook-cert-path", "/tmp/k8s-webhook-server/serving-certs", "the directory that contains the webhook key and certificate")
+	fs.StringVar(&c.WebhookCertName, "webhook-cert-name", "tls.crt", "the webhook server certificate name")
+	fs.StringVar(&c.WebhookCertKey, "webhook-cert-key", "tls.key", "the webhook server key name")
+	fs.BoolVar(&c.EnableLeaderElection, "leader-elect", false, "enable leader election for the controller manager")
 	fs.BoolVar(&c.SecureMetrics, "secureMetrics", false, "enables serving metrics via https")
 	fs.BoolVar(&c.EnableHTTP2, "enableHTTP2", false, "enable HTTP2")
 }
